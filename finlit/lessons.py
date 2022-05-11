934570be81aed7a2c67688dd10ed87d8b8b14df2
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2022-05-11 00:16:42 -0500

    refactor: change budgeting and saving lesson to just budgeting
    Replace all instances of term 'budgeting and saving' to just budgeting,
    as the two concepts will be taught seperately.

--- finlit/lessons.py
+++ finlit/lessons.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("lessons", __name__, url_prefix="/lessons")
 
-@bp.route("/budgeting-and-saving/")
-def budgeting_and_saving():
-    return render_template("lessons/budgeting_and_saving.html")
+@bp.route("/budgeting/")
+def budgeting():
+    return render_template("lessons/budgeting.html")
