dd6f4792c6286c72f36b68e7a45df1cc793e6ea3
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2022-05-11 22:44:30 -0500

    feat: complete 'saving' lesson article

--- finlit/lessons.py
+++ finlit/lessons.py
@@ -5,3 +5,7 @@ bp = Blueprint("lessons", __name__, url_prefix="/lessons")
 @bp.route("/budgeting/")
 def budgeting():
     return render_template("lessons/budgeting.html")
+
+@bp.route("/saving/")
+def saving():
+    return render_template("lessons/saving.html")
