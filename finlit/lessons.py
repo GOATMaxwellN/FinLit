defb4be2c7104ab47c4011d0cec4c4e94bc94370
Maxwell Nganyadi <maxwelltechmaster@outlook.com>
2022-05-12 21:30:48 -0500

    feat: create lesson articles for debt and more
    Lesson articles created are for debt, credit cards, line of credits and
    mortgage. Little or no content in the articles.

--- finlit/lessons.py
+++ finlit/lessons.py
@@ -9,3 +9,19 @@ def budgeting():
 @bp.route("/saving/")
 def saving():
     return render_template("lessons/saving.html")
+
+@bp.route("/debt/")
+def debt():
+    return render_template("lessons/debt.html")
+
+@bp.route("/credit-cards/")
+def credit_cards():
+    return render_template("lessons/credit-cards.html")
+
+@bp.route("/line-of-credit/")
+def line_of_credit():
+    return render_template("lessons/line-of-credit.html")
+
+@bp.route("/mortgage/")
+def mortgage():
+    return render_template("lessons/mortgage.html")
